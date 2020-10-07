e573a4048bad1b199c1ba103380af9959add507b
Unknown <mattmaryanski@gmail.com>
2020-10-07 12:41:34 -0400

    Add Unobtanium Vibranium recipes
    This is a WIP feature that we will be adding / removing more

---
+++ kubejs/server_scripts/u_recipes.js
@@ -0,0 +1,174 @@
+events.listen('recipes', function (e) {
+    //Unobtanium 
+     e.shapeless('allthemodium:vibranium_unobtainium_alloy_dust',['allthemodium:unobtainium_dust','allthemodium:vibranium_dust'])
+	 e.shapeless('allthemodium:unobtainium_allthemodium_alloy_dust',['allthemodium:unobtainium_dust','allthemodium:allthemodium_dust'])
+	 e.shapeless('allthemodium:vibranium_allthemodium_alloy_dust',['allthemodium:allthemodium_dust','allthemodium:vibranium_dust'])
+     e.shaped(item.of('alltheores:ore_aluminum', 12), [
+        ' UU',
+        'UUU',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_copper', 12), [
+        'U U',
+        'UUU',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_lead', 12), [
+        'UU ',
+        'UUU',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_nickel', 12), [
+        'UUU',
+        ' UU',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_osmium', 12), [
+        'UUU',
+        'U U',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_platinum', 12), [
+        'UUU',
+        'UU ',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_silver', 12), [
+        'UUU',
+        'UUU',
+        ' UU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_tin', 12), [
+        'UUU',
+        'UUU',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_uranium', 12), [
+        'UUU',
+        'UUU',
+        'UU '
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('alltheores:ore_zinc', 12), [
+        ' UU',
+        'UUU',
+        'UU '
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:gold_ore', 12), [
+        ' UU',
+        'UUU',
+        ' UU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:iron_ore', 12), [
+        'UU ',
+        'UUU',
+        'UU '
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:nether_quartz_ore', 12), [
+        'UUU',
+        'UUU',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:redstone', 16), [
+        'U U',
+        'U  ',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:glowstone_dust', 16), [
+        'U U',
+        '  U',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:end_stone', 32), [
+        'U U',
+        'UUU',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:slime_ball', 16), [
+        'U U',
+        '   ',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('industrialforegoing:plastic', 4), [
+        'U U',
+        'U U',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:white_wool', 16), [
+        ' U ',
+        'U U',
+        ' U '
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:obsidian', 16), [
+        'UUU',
+        'U U',
+        'UUU'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:oak_log', 32), [
+        ' U ',
+        'UUU',
+        ' U '
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:bone', 8), [
+        '  U',
+        ' U ',
+        'U  '
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:leather', 4), [
+        'U  ',
+        ' U ',
+        '  U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+     e.shaped(item.of('minecraft:gunpowder', 16), [
+        'U U',
+        ' U ',
+        'U U'
+    ], {
+        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    })
+})
