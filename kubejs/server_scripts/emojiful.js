ff035718cdac82680296ecbbd7feeebe2a379954
White_Phant0m <33400200+WhitePhant0m@users.noreply.github.com>
2020-10-20 14:42:07 +0300

    changed emojiful datapack into KubeJS script

---
+++ kubejs/server_scripts/emojiful.js
@@ -0,0 +1,33 @@
+events.listen('recipes', function (e) {
+    function emote(name, url) {
+        e.recipes.emojiful.emoji_recipe({
+            'category': 'emotes',
+            'name': name,
+            'url': url,
+        })
+    }
+    emote('asevil', 'https://cdn.discordapp.com/emojis/581816328260943872.gif')
+    emote('cattt', 'https://cdn.discordapp.com/emojis/642569163037999124.png')
+    emote('concern', 'https://cdn.discordapp.com/emojis/728377231163916378.png')
+    emote('despair', 'https://cdn.discordapp.com/emojis/730910956934135934.png')
+    emote('echpat', 'https://cdn.discordapp.com/emojis/745068176760504400.gif')
+    emote('hahayes', 'https://cdn.discordapp.com/emojis/446368345067880468.png')
+    emote('hmmm', 'https://cdn.discordapp.com/emojis/609911633078124558.png')
+    emote('idontgetit', 'https://cdn.discordapp.com/emojis/469912332303204359.png')
+    emote('ireallydontgetit', 'https://cdn.discordapp.com/emojis/707609930877763645.png')
+    emote('jeffthink', 'https://cdn.discordapp.com/emojis/642567793895800874.png')
+    emote('kek', 'https://cdn.discordapp.com/emojis/644421782924689408.png')
+    emote('myaaa', 'https://cdn.discordapp.com/emojis/699657454081802262.gif')
+    emote('nolike', 'https://cdn.discordapp.com/emojis/607808795904704514.png')
+    emote('noooooooooo', 'https://cdn.discordapp.com/emojis/594484860564013076.png')
+    emote('pogger', 'https://cdn.discordapp.com/emojis/612407807265865751.gif')
+    emote('popech', 'https://cdn.discordapp.com/emojis/606702943000854529.gif')
+    emote('preech', 'https://cdn.discordapp.com/emojis/587353977222594577.png')
+    emote('sadcat', 'https://cdn.discordapp.com/emojis/716615267328720897.gif')
+    emote('sheep', 'https://cdn.discordapp.com/emojis/642567723406327839.gif')
+    emote('smh', 'https://cdn.discordapp.com/emojis/400149002932453376.gif')
+    emote('stevedab', 'https://cdn.discordapp.com/emojis/586645062947962890.png')
+    emote('thinkaboutit', 'https://cdn.discordapp.com/emojis/642567832143396876.png')
+    emote('thinkeyes', 'https://cdn.discordapp.com/emojis/642567846597230614.png')
+    emote('thonk', 'https://cdn.discordapp.com/emojis/690375699453706250.png')
+})
