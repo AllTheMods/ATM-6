c1f8d1cdcda2ea3ec8ee521b3a74036143ad1cc5
White_Phant0m <33400200+WhitePhant0m@users.noreply.github.com>
2020-10-21 11:59:59 +0300

    made a uu matter item, gave it some special recipes

--- kubejs/server_scripts/u_recipes.js
+++ kubejs/server_scripts/u_recipes.js
@@ -3,127 +3,157 @@ events.listen('recipes', function (e) {
     e.shapeless('allthemodium:vibranium_unobtainium_alloy_dust', ['allthemodium:unobtainium_dust', 'allthemodium:vibranium_dust'])
     e.shapeless('allthemodium:unobtainium_allthemodium_alloy_dust', ['allthemodium:unobtainium_dust', 'allthemodium:allthemodium_dust'])
     e.shapeless('allthemodium:vibranium_allthemodium_alloy_dust', ['allthemodium:allthemodium_dust', 'allthemodium:vibranium_dust'])
-    var vuad = {
-        'U': 'allthemodium:vibranium_unobtainium_alloy_dust'
+    e.recipes.mekanism.nucleosynthesizing({
+        'itemInput': {
+            'ingredient': {
+                'tag': 'forge:dusts/vibranium_unobtainium_alloy'
+            }
+        },
+        'gasInput': {
+            'gas': 'mekanism:antimatter',
+            'amount': 1
+        },
+        'output': {
+            'item': 'kubejs:uu_matter',
+            'count': 16
+        },
+        'duration': 20
+    })
+    e.recipes.psi.trick_crafting({
+        'input': {
+            'tag': 'forge:dusts/vibranium_unobtainium_alloy'
+        },
+        'output': {
+            'type': 'forge:nbt',
+            'item': 'kubejs:uu_matter',
+            'count': 4
+        },
+        'cad': {
+            'item': 'psi:cad_assembly_psimetal'
+        },
+        'trick': 'psi:trick_ebony_ivory'
+    })
+    var uMatter = {
+        'U': 'kubejs:uu_matter'
     }
     e.shaped(item.of('alltheores:ore_aluminum', 12), [
         ' UU',
         'UUU',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_copper', 12), [
         'U U',
         'UUU',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_lead', 12), [
         'UU ',
         'UUU',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_nickel', 12), [
         'UUU',
         ' UU',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_osmium', 12), [
         'UUU',
         'U U',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_platinum', 12), [
         'UUU',
         'UU ',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_silver', 12), [
         'UUU',
         'UUU',
         ' UU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_tin', 12), [
         'UUU',
         'UUU',
         'U U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_uranium', 12), [
         'UUU',
         'UUU',
         'UU '
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('alltheores:ore_zinc', 12), [
         ' UU',
         'UUU',
         'UU '
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:gold_ore', 12), [
         ' UU',
         'UUU',
         ' UU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:iron_ore', 12), [
         'UU ',
         'UUU',
         'UU '
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:nether_quartz_ore', 12), [
         'UUU',
         'UUU',
         'U U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:redstone', 16), [
         'U U',
         'U  ',
         'U U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:glowstone_dust', 16), [
         'U U',
         '  U',
         'U U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:end_stone', 32), [
         'U U',
         'UUU',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:slime_ball', 16), [
         'U U',
         '   ',
         'U U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('industrialforegoing:plastic', 4), [
         'U U',
         'U U',
         'U U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:white_wool', 16), [
         ' U ',
         'U U',
         ' U '
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:obsidian', 16), [
         'UUU',
         'U U',
         'UUU'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:oak_log', 32), [
         ' U ',
         'UUU',
         ' U '
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:bone', 8), [
         '  U',
         ' U ',
         'U  '
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:leather', 4), [
         'U  ',
         ' U ',
         '  U'
-    ], vuad)
+    ], uMatter)
     e.shaped(item.of('minecraft:gunpowder', 16), [
         'U U',
         ' U ',
         'U U'
-    ], vuad)
+    ], uMatter)
 })
--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -2,6 +2,7 @@ events.listen('item.registry', function (e) {
   e.create('mass_of_wills').displayName('§6Mass of Wills').tooltip('§8Combination of the six wills of the ancients').unstackable()
   e.create('rune_of_sins').displayName('§6Rune of Sins').tooltip('§8Rune that contains the power of the seven sins').unstackable()
   e.create('rotten_leather').displayName('Rotten Leather')
+  e.create('uu_matter').displayName('§dUU-Matter')
   /* e.create('example_food').food(function(food){
     food.hunger(10)
     food.saturation(1)
@@ -13,5 +14,5 @@ events.listen('item.registry', function (e) {
 })
 events.listen('block.registry', function (e) {
   e.create('magical_soil').displayName('§bMagical Soil').material(block.material.organic).hardness(0.6)
-  e.create('nether_star_block').displayName('§eNether Star Block').material(block.material.glass).hardness(2)
+  e.create('nether_star_block').displayName('§eNether Star Block').material(block.material.glass).hardness(2).lightLevel(1)
 })
