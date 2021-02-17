8ff6d79b5fd1f4e647c5834abbe609a7c4aa9a8c
Paul <68764896+Pdiddy973@users.noreply.github.com>
2021-02-17 12:50:35 -0500

    Update removals.js

--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -21,8 +21,6 @@ events.listen('recipes', e => {
         'xreliquary:alkahestry/crafting/nether_star',
 
         'thermal:machine/press/packing2x2/press_honeycomb_packing',
-
-        'thermal:machine/press/unpacking/press_honeycomb_unpacking',
         'thermal:machine/press/unpacking/press_honeycomb_unpacking',
         
         'ctiers:centrifuge_casing_tier_creative',
