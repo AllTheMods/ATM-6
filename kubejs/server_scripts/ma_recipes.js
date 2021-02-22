282354d6f63498da974575420aeaca9af7c58f8c
Pdiddy973 <Paulirwin973@outlook.com>
2021-02-21 22:44:45 -0500

    MA Balance
    Removes "compounds" like bees

--- kubejs/server_scripts/ma_recipes.js
+++ kubejs/server_scripts/ma_recipes.js
@@ -1,4 +1,4 @@
-events.listen('recipes', function (e) {
+events.listen('recipes', function(e) {
     //Functions
     function plus(result, count, name) {
         e.shaped(item.of(result, count), [
@@ -29,16 +29,11 @@ events.listen('recipes', function (e) {
         'E': 'mysticalagriculture:nature_essence'
     })
 
-    circle('rats:oratchalcum_ingot', 4, 'oratchalcum')
-    circle('industrialforegoing:dryrubber',6,'rubber')
-
     circle('allthemodium:allthemodium_nugget', 1, 'allthemodium')
     circle('allthemodium:vibranium_nugget', 1, 'vibranium')
     circle('allthemodium:unobtainium_nugget', 1, 'unobtainium')
 
     circle('silentgear:azure_electrum_ingot', 4, 'azure_electrum')
-    circle('silentgear:azure_silver_ingot', 6, 'azure_silver')
     circle('silentgear:crimson_iron_ingot', 6, 'crimson_iron')
-    circle('silentgear:crimson_steel_ingot', 4, 'crimson_steel')
     circle('mekanism:fluorite_gem', 12, 'fluorite')
 })
