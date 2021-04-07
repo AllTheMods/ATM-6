301d9460b56882fc56585268305dea5a4e9d96cc
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2021-04-07 23:58:16 +0200

    Fixed inconsitencies

--- kubejs/server_scripts/creative.js
+++ kubejs/server_scripts/creative.js
@@ -39,21 +39,21 @@ events.listen('recipes', function(e) {
         'MCM',
         'TAT'
     ], {
-        'H': 'compressium:honey_6',
-        'M': 'mekanism:pellet_antimatter',
-        'C': 'ctiers:centrifuge_controller_tier_creative',
-        'T': 'resourcefulbees:t4_apiary',
-        'A': 'allthemodium:unobtainium_allthemodium_alloy_block'
+        H: 'compressium:honey_6',
+        M: 'mekanism:pellet_antimatter',
+        C: 'ctiers:centrifuge_controller_tier_creative',
+        T: 'resourcefulbees:t4_apiary',
+        A: 'allthemodium:unobtainium_allthemodium_alloy_block'
     })
     e.shaped('creativeapiary:creative_apiary_storage', [
         'HNH',
         'CAC',
         'NHN'
     ], {
-        'H': 'compressium:honey_3',
-        'N': 'kubejs:nether_star_block',
-        'C': 'ctiers:centrifuge_casing_tier_creative',
-        'A': 'resourcefulbees:apiary_storage'
+        H: 'compressium:honey_3',
+        N: 'kubejs:nether_star_block',
+        C: 'ctiers:centrifuge_casing_tier_creative',
+        A: 'resourcefulbees:apiary_storage'
     })
     e.shaped('creativeapiary:creative_apiary_breeder', [
         'BNB',
