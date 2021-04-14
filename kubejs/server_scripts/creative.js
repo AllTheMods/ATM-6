08f52af4552ae652587a7e4c8d04ede2df2ed091
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2021-04-15 01:15:37 +0200

    Creative recipes
    * Added creative rat upgrade
    
    * Added creative rat upgrade combined
    
    * Fixed inconsistencies
    
    * Grouped up ctiers correctly
    
    * Added recipe for powah creative energy cell
    
    * Added creative thermal upgrade
    
    * clean up

--- kubejs/server_scripts/creative.js
+++ kubejs/server_scripts/creative.js
@@ -33,36 +33,6 @@ events.listen('recipes', function(e) {
         D: 'compressium:diamond_3',
         E: 'compressium:emerald_3'
     })
-    //CApiary
-    e.shaped('creativeapiary:tcreative_apiary', [
-        'HMH',
-        'MCM',
-        'TAT'
-    ], {
-        'H': 'compressium:honey_6',
-        'M': 'mekanism:pellet_antimatter',
-        'C': 'ctiers:centrifuge_controller_tier_creative',
-        'T': 'resourcefulbees:t4_apiary',
-        'A': 'allthemodium:unobtainium_allthemodium_alloy_block'
-    })
-    e.shaped('creativeapiary:creative_apiary_storage', [
-        'HNH',
-        'CAC',
-        'NHN'
-    ], {
-        'H': 'compressium:honey_3',
-        'N': 'kubejs:nether_star_block',
-        'C': 'ctiers:centrifuge_casing_tier_creative',
-        'A': 'resourcefulbees:apiary_storage'
-    })
-    e.shaped('creativeapiary:creative_apiary_breeder', [
-        'BNB',
-        'NBN',
-        'BNB'
-    ], {
-        'B': 'resourcefulbees:apiary_breeder',
-        'N': 'kubejs:nether_star_block'
-    })
     e.shaped('ctiers:centrifuge_controller_tier_creative', [
         'ICG',
         'HSH',
@@ -76,6 +46,36 @@ events.listen('recipes', function(e) {
         E: 'compressium:emerald_5',
         H: 'compressium:honey_5'
     })
+    //CApiary
+    e.shaped('creativeapiary:tcreative_apiary', [
+        'HMH',
+        'MCM',
+        'TAT'
+    ], {
+        H: 'compressium:honey_6',
+        M: 'mekanism:pellet_antimatter',
+        C: 'ctiers:centrifuge_controller_tier_creative',
+        T: 'resourcefulbees:t4_apiary',
+        A: 'allthemodium:unobtainium_allthemodium_alloy_block'
+    })
+    e.shaped('creativeapiary:creative_apiary_storage', [
+        'HNH',
+        'CAC',
+        'NHN'
+    ], {
+        H: 'compressium:honey_3',
+        N: 'kubejs:nether_star_block',
+        C: 'ctiers:centrifuge_casing_tier_creative',
+        A: 'resourcefulbees:apiary_storage'
+    })
+    e.shaped('creativeapiary:creative_apiary_breeder', [
+        'BNB',
+        'NBN',
+        'BNB'
+    ], {
+        B: 'resourcefulbees:apiary_breeder',
+        N: 'kubejs:nether_star_block'
+    })
     //Rats
     e.shaped('rats:creative_cheese', [
         'CCC',
@@ -83,6 +83,24 @@ events.listen('recipes', function(e) {
         'CCC'
     ], {
         C: '#forge:cheese_block',
+        S: atm_star
+    })
+    e.shaped('rats:rat_upgrade_creative', [
+        'CUC',
+        'USU',
+        'CUC'
+    ], {
+        C: '#forge:cheese_block',
+        U: 'rats:rat_upgrade_block',
+        S: atm_star
+    })
+    e.shaped('rats:rat_upgrade_combined_creative', [
+        'RUR',
+        'USU',
+        'RUR'
+    ], {
+        R: 'rats:idol_of_ratlantis',
+        U: 'rats:rat_upgrade_combined',
         S: atm_star
     })
     //Woot
@@ -116,30 +134,32 @@ events.listen('recipes', function(e) {
         mode: 0,
         spells: ",intangible,ignite,flare,strength,craft,cold_snap,rune,snare,slowfall,freeze,split,crush,smelt,summon_steed,accelerate,summon_vex,lightning,grow,dampen,touch,invisibility,extract,delay,light,duration_down,exchange,place_block,summon_wolves,shield,conjure_water,cut,harm,interact,blink,amplify,phantom_block,fell,extend_time,heal,leap,redstone_signal,pierce,harvest,fortune,break,pickup,launch,dispel,haste,ender_inventory,pull,explosion,fangs,aoe,gravity,self,aquatic,projectile,wither,gust"
     }), [
-        'ABC',
-        'CCD',
-        'DDE'
+        'BSG',
+        'GGE',
+        'EER'
     ], {
-        A: 'ars_nouveau:archmage_spell_book',
-        B: atm_star,
-        C: 'astralsorcery:resonating_gem',
-        D: 'minecraft:ender_eye',
-        E: 'tombstone:soul_receptacle'
+        B: 'ars_nouveau:archmage_spell_book',
+        S: atm_star,
+        G: 'astralsorcery:resonating_gem',
+        E: 'minecraft:ender_eye',
+        R: 'tombstone:soul_receptacle'
     })
 
     e.shaped('ars_nouveau:creative_mana_jar', [
-        'ACA',
-        'ABA',
-        'AAA'
+        'GMG',
+        'GSG',
+        'GGG'
     ], {
-        A: '#forge:glass',
-        B: atm_star,
-        C: 'ars_nouveau:mana_gem_block'
+        G: '#forge:glass',
+        S: atm_star,
+        M: 'ars_nouveau:mana_gem_block'
     })
     //Immersive Engineering
     energize(['immersiveengineering:capacitor_lv', 'immersiveengineering:capacitor_mv', 'immersiveengineering:capacitor_hv', atm_star], 'immersiveengineering:capacitor_creative', 2147483647)
     //Integrated Dynamics
-    energize(['integrateddynamics:coal_generator', 'integrateddynamics:coal_generator', atm_star, 'integrateddynamics:coal_generator', 'integrateddynamics:coal_generator'], 'integrateddynamics:energy_battery_creative', 2147483647)
+    energize(['integrateddynamics:energy_battery', 'integrateddynamics:energy_battery', atm_star, 'integrateddynamics:energy_battery', 'integrateddynamics:energy_battery'], 'integrateddynamics:energy_battery_creative', 2147483647)
+    //Powah
+    energize(['powah:energy_cell_nitro', 'powah:energy_cell_nitro', atm_star, 'powah:energy_cell_nitro','powah:energy_cell_nitro'], 'powah:energy_cell_creative', 2147483647)
     //Applied Energistics 2
     energize(['appliedenergistics2:dense_energy_cell', atm_star], 'appliedenergistics2:creative_energy_cell', 2147483647)
     //Mekanism
@@ -164,15 +184,15 @@ events.listen('recipes', function(e) {
     }), 2147483647)
     //Create
     e.recipes.create.mechanical_crafting('create:creative_motor', [
-        'ABA',
-        'CDC',
-        'BEB'
+        'CLC',
+        'HSH',
+        'LGL'
     ], {
-        A: 'create:cogwheel',
-        B: 'create:large_cogwheel',
-        C: 'create:shaft',
-        D: atm_star,
-        E: 'create:gearbox'
+        C: 'create:cogwheel',
+        L: 'create:large_cogwheel',
+        H: 'create:shaft',
+        S: atm_star,
+        G: 'create:gearbox'
     })
     //Refined Storage
     energize(['refinedstorage:wireless_grid'], 'refinedstorage:creative_wireless_grid', 2147483647)
@@ -230,22 +250,33 @@ events.listen('recipes', function(e) {
     ], 'pneumaticcraft:creative_compressor', 1, 4.9)
     //Storage Drawers
     e.shaped('16x storagedrawers:creative_storage_upgrade', [
-        'AAA',
-        'CBC',
-        'AAA'
+        'EEE',
+        'ASA',
+        'EEE'
     ], {
-        A: 'storagedrawers:emerald_storage_upgrade',
-        B: atm_star,
-        C: 'allthemodium:allthemodium_block'
+        E: 'storagedrawers:emerald_storage_upgrade',
+        S: atm_star,
+        A: 'allthemodium:allthemodium_block'
+    })
+    //Thermal 
+    e.shaped('thermal:machine_efficiency_creative_augment', [
+        'AHA',
+        'ESE',
+        'AHA'
+    ], {
+        A: 'thermal:upgrade_augment_3',
+        H: Item.of('minecraft:enchanted_book').enchant('cofh_core:holding', 4),
+        E: Item.of('minecraft:enchanted_book').enchant('minecraft:efficiency', 5),
+        S: atm_star
     })
     //Ice And Fire
     e.shaped('2x iceandfire:creative_dragon_meal', [
-        'AAA',
-        'ABA',
-        'AAA'
+        'DDD',
+        'DSD',
+        'DDD'
     ], {
-        A: 'iceandfire:dragon_meal',
-        B: atm_star
+        D: 'iceandfire:dragon_meal',
+        S: atm_star
     })
 
     const colors = ['red', 'green', 'bronze', 'gray', 'blue', 'white', 'sapphire', 'silver', 'electric', 'amythest', 'copper', 'black']
