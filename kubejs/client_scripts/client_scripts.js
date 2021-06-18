9a2045ed8e355086bd06f0418c1ac636727ce551
Relentless <relentless@rlnt.dev>
2021-06-18 06:44:17 +0200

    removed obsolete bitumen from JEI

--- kubejs/client_scripts/client_scripts.js
+++ kubejs/client_scripts/client_scripts.js
@@ -118,6 +118,7 @@ onEvent(`jei.hide.items`, e => {
         /immersiveengineering:.+coke/,
         `immersiveengineering:coke`,
         `immersiveengineering:slag`,
+        'immersiveengineering:bitumen',
 
         `darkutils:ender_hopper`,
 
