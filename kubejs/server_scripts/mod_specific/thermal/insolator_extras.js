36e1c7698e3cbeab9144dbfa78b6c2a0d2279567
nddragoon <54559451+nddragoon@users.noreply.github.com>
2021-09-19 22:32:30 -0500

    Create script for insolator_extras.js
    Add insolator recipes for all craftable seeds that dont have it

--- kubejs/server_scripts/mod_specific/thermal/insolator_extras.js
+++ kubejs/server_scripts/mod_specific/thermal/insolator_extras.js
@@ -1,3 +1,31 @@
 onEvent('recipes', e => {
-	
+	//Add any extra seeds missing from the insolator.
+	//For example, 'apatite' will create a recipe of apatite_seeds = apatite_essence + apatite_seeds
+	const listMARecipes = [
+	'apatite', 
+	'slimesteel', 
+	'pig_iron',
+	'steeleaf',
+	'ironwood',
+	'aquamarine',
+	'azure_silver',
+	'crimson_iron',
+	'ruby',
+	'sapphire',
+	'cobalt',
+	'rose_gold',
+	'fluorite',
+	'knightmetal',
+	'fiery_ingot',
+	'azure_electrum',
+	'manyullyn',
+	'queens_slime',
+	'hepatizon',
+	'draconium'
+	]
+	for (let recipe of listMARecipes) {
+		var essence = `mysticalagriculture:${recipe}_essence`
+		var seeds = `mysticalagriculture:${recipe}_seeds`
+		e.recipes.thermal.insolator([essence,seeds],seeds).water(500)
+	}
 })
