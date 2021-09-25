b6e2dc2e0032e92f48b64f4e61118cd0ab02c170
Dawn Walker <nosirrah.reklaw@gmail.com>
2021-09-25 14:40:54 +1000

    Update byg.js
    Added black ice recipe

--- kubejs/server_scripts/mod_specific/byg/byg.js
+++ kubejs/server_scripts/mod_specific/byg/byg.js
@@ -1,4 +1,9 @@
 onEvent('recipes', e => {
+  e.shaped('byg:black_ice', ['III', 'IDI', 'III'], {
+    I: 'minecraft:ice',
+    D: '#forge:dyes/black'
+  }).id(`kubejs:byg_black_ice`)
+
   removeRecipeByID(e, [
     'byg:compat/tconstruct/magma_cream_from_cryptic_magma_block_melting',
     'byg:compat/tconstruct/magma_cream_from_magmatic_stone_melting',
