e932e2d6f2f5f68c5aa8d9b2461a66607f2f053a
Lexxie <30494658+ladylexxie@users.noreply.github.com>
2021-09-26 17:04:38 +0100

    Update storage_drawers.js

--- kubejs/server_scripts/mod_specific/storage_drawers/storage_drawers.js
+++ kubejs/server_scripts/mod_specific/storage_drawers/storage_drawers.js
@@ -4,4 +4,7 @@ onEvent('recipes', e => {
     S: 'atmadditions:atm_star',
     A: 'allthemodium:allthemodium_block'
   })
+  e.replaceInput({ output: 'storagedrawers:compacting_drawers_3', type: 'minecraft:crafting_shaped' }, 'minecraft:stone', '#forge:stone')
+  e.replaceInput({ output: 'storagedrawers:controller', type: 'minecraft:crafting_shaped' }, 'minecraft:stone', '#forge:stone')
+  e.replaceInput({ output: 'storagedrawers:controller_slave', type: 'minecraft:crafting_shaped' }, 'minecraft:stone', '#forge:stone')
 })
