17598011e3cbc57cfbe0b236856fa1bab1488754
Lexxie <30494658+ladylexxie@users.noreply.github.com>
2021-10-03 15:01:40 +0100

    Update jei_removals.js

--- kubejs/client_scripts/jei_removals.js
+++ kubejs/client_scripts/jei_removals.js
@@ -125,7 +125,7 @@ onEvent('jei.hide.items', e => {
     'thermal:rf_coil_creative_augment',
     'thermal:fluid_tank_creative_augment',
     'thermal:machine_catalyst_creative_augment',
-	'mekanism:dust_sulfur'
+		'mekanism:dust_sulfur'
   ])
 
   enviroStonesMats.forEach(stone => e.hide(`/enviromats:${stone}_.*/`))
