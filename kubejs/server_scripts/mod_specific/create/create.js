0502f9ec10b7ddd2d399009da4718f0012cc3fe9
AR.E <52563078+AR-E@users.noreply.github.com>
2021-10-06 21:22:54 +0300

    Update create.js
    add ancient debris processing

--- kubejs/server_scripts/mod_specific/create/create.js
+++ kubejs/server_scripts/mod_specific/create/create.js
@@ -19,6 +19,15 @@ onEvent('recipes', e => {
     'createaddition:honey_cake'
   ]).superheated().processingTime(6000).id('kubejs:create/creative_blaze_cake')
 
+  e.recipes.create.crushing([
+    '2x minecraft:netherite_scrap',
+    Item.of('minecraft:netherite_scrap', 2).withChance(0.33)
+  ], 'minecraft:ancient_debris')
+
+  e.recipes.create.milling(
+    '2x minecraft:netherite_scrap',
+    'minecraft:ancient_debris')
+  
   e.recipes.createSplashing([
     '10x alltheores:platinum_nugget',
     Item.of('alltheores:platinum_nugget', 5).withChance(0.5)
