47f8a67a8c0a6e6302e094733c0019a132bbfe90
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2021-10-28 23:02:53 -0400

    add tooltip to ender tank too

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -63,6 +63,7 @@ onEvent('item.tooltip', e => {
   formattedTooltip('appliedenergistics2:quantum_entangled_singularity', 'To make this, you must use Ender Pearls, not Ender Dust')
   formattedTooltip('astralsorcery:altar_discovery', 'Check the Astral Tome for more info')
   formattedTooltip('enderchests:ender_chest', 'Increase the capacity by shift right-clicking with Eye of Ender or Ender Pearls')
+  formattedTooltip('enderchests:ender_tank', 'Increase the capacity by shift right-clicking with Eye of Ender or Ender Pearls')
 
   //#region Pipez
   //pipes
