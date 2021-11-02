9732feb10061c3e5d806cee48fd4e2feb02b5c38
Pdiddy973 <68764896+Pdiddy973@users.noreply.github.com>
2021-11-02 19:05:11 -0400

    Update powah.js
    - add recipe ID to nbt rest
    - change reactor to a upgrade method like other powah items
    - remove crafting recipe for basic energy cell to keep with just upgrade system

--- kubejs/server_scripts/mod_specific/powah/powah.js
+++ kubejs/server_scripts/mod_specific/powah/powah.js
@@ -1,8 +1,42 @@
 onEvent('recipes', e => {
   utils.listOf(['energy_cell', 'reactor', 'furnator', 'magmator', 'thermo_generator', 'solar_panel', 'player_transmitter', 'energy_hopper', 'energy_discharger', 'energizing_rod']).forEach(type => {
     utils.listOf(['starter', 'basic', 'hardened', 'blazing', 'niotic', 'spirited', 'nitro']).forEach(tier => {
-      e.shapeless(item.of(`powah:${type}_${tier}`), `powah:${type}_${tier}`)
+      e.shapeless(item.of(`powah:${type}_${tier}`), `powah:${type}_${tier}`).id(`kubejs:powah/nbt_reset/${type}/${tier}`)
     })
   })
   energize(e, ['powah:energy_cell_nitro', 'powah:energy_cell_nitro', 'atmadditions:atm_star', 'powah:energy_cell_nitro', 'powah:energy_cell_nitro'], 'powah:energy_cell_creative', '2147483647')
+
+  modifyShaped(e, 'powah:reactor_basic', 4, ['RCR', 'CDC', 'RCR'], {
+    R: 'powah:reactor_starter',
+    C: 'powah:capacitor_basic_large',
+    D: 'powah:dielectric_casing'
+  })
+  modifyShaped(e, 'powah:reactor_hardened', 4, ['RCR', 'CDC', 'RCR'], {
+    R: 'powah:reactor_basic',
+    C: 'powah:capacitor_hardened',
+    D: 'powah:dielectric_casing'
+  })
+  modifyShaped(e, 'powah:reactor_blazing', 4, ['RCR', 'CDC', 'RCR'], {
+    R: 'powah:reactor_hardened',
+    C: 'powah:capacitor_blazing',
+    D: 'powah:dielectric_casing'
+  })
+  modifyShaped(e, 'powah:reactor_niotic', 4, ['RCR', 'CDC', 'RCR'], {
+    R: 'powah:reactor_blazing',
+    C: 'powah:capacitor_niotic',
+    D: 'powah:dielectric_casing'
+  })
+  modifyShaped(e, 'powah:reactor_spirited', 4, ['RCR', 'CDC', 'RCR'], {
+    R: 'powah:reactor_niotic',
+    C: 'powah:capacitor_spirited',
+    D: 'powah:dielectric_casing'
+  })
+  modifyShaped(e, 'powah:reactor_nitro', 4, ['RCR', 'CDC', 'RCR'], {
+    R: 'powah:reactor_spirited',
+    C: 'powah:capacitor_nitro',
+    D: 'powah:dielectric_casing'
+  })
+  removeRecipeByID(e, [
+    'powah:crafting/energy_cell_basic'
+  ])
 })
