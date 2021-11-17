f23800ed0e8a29cbdf1af3d8756d33b5fbfb9c0a
Pdiddy973 <68764896+Pdiddy973@users.noreply.github.com>
2021-11-17 18:37:52 -0500

    Update iceandfire.js
    - adds lily crafting

--- kubejs/server_scripts/mod_specific/iceandfire/iceandfire.js
+++ kubejs/server_scripts/mod_specific/iceandfire/iceandfire.js
@@ -1,11 +1,26 @@
 onEvent('recipes', e => {
-  let dragonColors = ['red', 'green', 'bronze', 'gray', 'blue', 'white', 'sapphire', 'silver', 'electric', 'amythest', 'copper', 'black']
+  function dragonFlowers(dragons) {
+    dragons.forEach(dragon => {
+      e.shaped(Item.of(`iceandfire:${dragon.equals('ice') ? 'frost' : dragon}_lily`, 4), [
+        'ACA',
+        'BDB',
+        'ACA'
+      ], {
+        A: 'minecraft:lily_of_the_valley',
+        B: `iceandfire:${dragon}_dragon_heart`,
+        C: `iceandfire:${dragon}_dragon_flesh`,
+        D: 'mysticalagriculture:master_infusion_crystal'
+      }).id(`kubejs:shaped/iceandfire/${dragon}_lily`)
+    })
+  }
+  dragonFlowers([`fire`, `lightning`, `ice`])
 
   e.shaped('2x iceandfire:creative_dragon_meal', ['DDD', 'DSD', 'DDD'], {
     D: 'iceandfire:dragon_meal',
     S: 'atmadditions:atm_star'
   })
 
+  let dragonColors = ['red', 'green', 'bronze', 'gray', 'blue', 'white', 'sapphire', 'silver', 'electric', 'amythest', 'copper', 'black']
   dragonColors.forEach(color => {
     e.shaped(`iceandfire:dragonegg_${color}`, ['DED', 'ESE', 'DED'], {
       S: 'atmadditions:atm_star',
