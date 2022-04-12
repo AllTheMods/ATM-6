05c6c71ac885b0fdf1988fa976287915a11ccca3
Jeremiah Winsley <jeremiah.winsley@gmail.com>
2022-04-11 20:59:54 -0400

    Fix slurry overlap

--- kubejs/server_scripts/mod_specific/_allthemods/alltheores.js
+++ kubejs/server_scripts/mod_specific/_allthemods/alltheores.js
@@ -1,12 +1,3 @@
 onEvent('recipes', e => {
-
-  const slurries = [
-    'uranium',
-    'copper',
-    'osmium',
-    'lead',
-    'tin'
-  ]
-
-  slurries.forEach(type => e.remove({id: `alltheores:mek_processing/${type}/slurry/dirty`}))
+  mekanismMetals.forEach(type => e.remove({id: `alltheores:mek_processing/${type}/slurry/dirty`}))
 });
