757ac6890658ed31e586e8196ab528b40b8e834b
Chris <chris.barwick16@gmail.com>
2022-05-29 19:10:37 -0700

    Implement ATM Tool Salvage Recipes

--- kubejs/server_scripts/mod_specific/silentsgear/silentsgear.js
+++ kubejs/server_scripts/mod_specific/silentsgear/silentsgear.js
@@ -11,13 +11,52 @@ onEvent('recipes', e => {
         })
     }
 
-    const nuggetRatio = {"helmet": 2, "chestplate": 4, "leggings": 3, "boots": 2}
+    const armorNuggetRatio = {"helmet": 2, "chestplate": 4, "leggings": 3, "boots": 2}
+    const toolNuggetRatio = {
+        "pick": {
+            "allthemodium": 2,
+            "vibranium": 3,
+            "unobtainium": 3,            
+        }, 
+        "axe": {
+            "allthemodium": 2,
+            "vibranium": 3,
+            "unobtainium": 3,
+        }, 
+        "sword": {
+            "allthemodium": 1, 
+            "vibranium": 2,
+            "unobtainium": 2,
+        }, 
+        "shovel": {
+            "allthemodium": 2, 
+            "vibranium": 1,
+            "unobtainium": 1,
+        },
+        "paxel": {
+            "allthemodium": 9,
+            "vibranium": 10,
+            "unobtainium": 10,
+        }
+    }
 
-    utils.listOf(['allthemodium', 'vibranium', 'unobtanium'])
+    utils.listOf(['allthemodium', 'vibranium', 'unobtainium'])
       .forEach(type => {
           utils.listOf(['helmet', 'chestplate', 'leggings', 'boots'])
             .forEach(armor => {
-                salvage(e, `allthemodium:${type}_${armor}`, [{item: `allthemodium:${type}_nugget`, count: nuggetRatio[armor]}])
+                salvage(e, `allthemodium:${type}_${armor}`, [{item: `allthemodium:${type}_nugget`, count: armorNuggetRatio[armor]}])
             })
+      })
+
+    utils.listOf(['pick', 'axe', 'sword', 'shovel', 'paxel'])
+      .forEach(tool => {
+        const result = [];
+
+        utils.listOf(['allthemodium', 'vibranium', 'unobtainium'])
+          .forEach(nugget => {
+            result.push({"item": `allthemodium:${nugget}_nugget`, "count": toolNuggetRatio[tool][nugget]})
+          })
+
+          salvage(e, `allthemodium:alloy_${tool}`, result)
       })
 })
