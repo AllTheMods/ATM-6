21785a6d90b192ee65c390e5f81687f283297166
Ruthar <101109168+Laithur@users.noreply.github.com>
2024-01-23 20:09:02 +0100

    New creative recipes
    Add 2 new uses, 1 for ATM star + some PSI materials to craft PSI creative CAD, and Creative Rune from Elementalcraft using atm shards

---
+++ kubejs/server_scripts/mod_specific/elementalcraft/elementalcraft.js
@@ -0,0 +1,27 @@
+onEvent.custom({
+  "type": "elementalcraft:inscription",
+  "element_type": "earth",
+  "element_amount": 10000,
+  "slate": {
+    "item": "elementalcraft:major_rune_slate"
+  },
+  "ingredients": [
+    {
+      "tag": "elementalcraft:gems/pristine_earth"
+    },
+    {
+      "item": "atmadditions:atm_star_shard"
+    },
+    {
+      "item": "atmadditions:atm_star_shard"
+    }
+  ],
+  "output": {
+    "item": "elementalcraft:rune",
+    "nbt": {
+      "elementalcraft": {
+        "rune": "elementalcraft:creative"
+      }
+    }
+  }
+})
---
+++ kubejs/server_scripts/mod_specific/psi/psi.js
@@ -0,0 +1,13 @@
+onEvent('recipes', e => {
+  e.shaped('psi:cad_assembly_creative',['ABC','DEF','GHI']{
+    A: 'psi:cad_core_hyperclocked',
+    B: 'psi:cad_assembly_ebony_psimetal',
+    C: 'psi:cad_core_radiative',
+    D: 'psi:cad_socket_huge',
+    E: 'atmadditions:atm_star',
+    F: 'psi:cad_socket_transmisive',
+    G: 'psi:cad_battery_ultradense',
+    H: 'psi:cad_assembly_ivory_psimetal',
+    I: 'psi:cad_battery_ultradense',
+  })
+})
